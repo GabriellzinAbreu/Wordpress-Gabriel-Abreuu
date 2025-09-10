e19ebdefd856e11d1b00aacf3689d86998c4511a
GabriellzinAbreu <gabriel.abreu.silva@escola.pr.gov.br>
2025-09-09 21:00:40 -0300

    Finalizando projeto

--- src/index.php
+++ src/index.php
@@ -15,3 +15,12 @@ define( 'WP_USE_THEMES', true );
 
 /** Loads the WordPress Environment and Template */
 require __DIR__ . '/wp-blog-header.php';
+
+$mysqli = new mysqli("mysql", "lampuser", "lamppass", "lampdb");
+if ($mysqli->connect_errno) {
+ echo "Falha na conexao: " . $mysqli->connect_error;
+ exit();
+}
+echo "Conexao bem-sucedida!\n";
+phpinfo();
+
